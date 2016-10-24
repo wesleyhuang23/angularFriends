515d0eee6878501ae65ee4947078cecd34509a37
wesleyhuang23 <wesleyhuang23@gmail.com>
2016-10-24 16:45:02 -0600

    angular friends v1

---
+++ js/controller.js
@@ -0,0 +1,48 @@
+angular.module('friendMachine').controller('friendCtrl', function($scope){
+
+  $scope.friends =[
+                  {
+                    name: 'Derek Jeter',
+                    location: 'Provo, Utah, United States',
+                    status: 'Does Utah have baseball?',
+                    numOfFriends: 12348
+                  },
+                  {
+                    name: 'Steven Speilberg',
+                    location: 'Beverly Hills, California, United States',
+                    status: 'Does Utah have Movies?',
+                    numOfFriends: 242975
+                  },
+                  {
+                    name: 'Barak Obama',
+                    location: 'Washington D.C, Maryland, United States',
+                    status: 'Does Utah have Politics?',
+                    numOfFriends: 242375
+                  },
+                  {
+                    name: 'Conan O" Brian',
+                    location: 'Beverly Hills, California, United States',
+                    status: 'Does Utah have talk shows?',
+                    numOfFriends: 2432975
+                  },
+                  {
+                    name: 'Taylor Swift',
+                    location: 'Beverly Hills, California, United States',
+                    status: 'Does Utah have Music?',
+                    numOfFriends: 2423975
+                  },
+                  {
+                    name: 'Elon Musk',
+                    location: 'Beverly Hills, California, United States',
+                    status: 'Does Utah have electric cars?',
+                    numOfFriends: 24332975
+                  },
+                ];
+
+                var sortBy;
+                var $scope.order = function(most){
+
+                };
+
+
+});
